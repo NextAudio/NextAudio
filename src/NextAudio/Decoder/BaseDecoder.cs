f3b3c5ef0cda6146cb6b9ddcdbab7d556ed976dd
Renan Pinheiro <renancamerapinheiro@gmail.com>
2020-11-27 20:46:10 -0300

    Decoder change Async API to Sync API

--- src/NextAudio/Decoder/BaseDecoder.cs
+++ src/NextAudio/Decoder/BaseDecoder.cs
@@ -28,37 +28,28 @@ namespace NextAudio
         public virtual int SampleRate { get; }
 
         /// <inheritdoc />
-        public abstract Task<int> DecodeAsync(byte[] inputBuffer, int inputOffset, int inputCount, byte[] outputBuffer, int outputOffset, int outputCount, CancellationToken cancellationToken = default);
+        public abstract int Decode(byte[] inputBuffer, int inputOffset, int inputCount, byte[] outputBuffer, int outputOffset, int outputCount);
 
         /// <inheritdoc />
-        public virtual ValueTask<int> DecodeAsync(ReadOnlyMemory<byte> inputBuffer, Memory<byte> outputBuffer, CancellationToken cancellationToken = default)
+        public virtual int Decode(ReadOnlySpan<byte> inputBuffer, Span<byte> outputBuffer)
         {
-            if (MemoryMarshal.TryGetArray(inputBuffer, out ArraySegment<byte> inputArray) && MemoryMarshal.TryGetArray(outputBuffer, out ArraySegment<byte> outputArray))
-                return new ValueTask<int>(DecodeAsync(inputArray.Array!, inputArray.Offset, inputArray.Count, outputArray.Array!, outputArray.Offset, outputArray.Count, cancellationToken));
-
             var sharedInputBuffer = ArrayPool<byte>.Shared.Rent(inputBuffer.Length);
-
-            inputBuffer.Span.CopyTo(sharedInputBuffer);
-
             var sharedOutputBuffer = ArrayPool<byte>.Shared.Rent(outputBuffer.Length);
 
-            return FinishDecodeAsync(DecodeAsync(sharedInputBuffer, 0, inputBuffer.Length, sharedOutputBuffer, 0, outputBuffer.Length, cancellationToken), sharedInputBuffer, sharedOutputBuffer, outputBuffer);
-        }
-
-        private static async ValueTask<int> FinishDecodeAsync(Task<int> decodeTask, byte[] localInputBuffer, byte[] localOutputBuffer, Memory<byte> localDestination)
-        {
             try
             {
-                var result = await decodeTask.ConfigureAwait(false);
+                inputBuffer.CopyTo(sharedInputBuffer);
 
-                new ReadOnlySpan<byte>(localOutputBuffer, 0, result).CopyTo(localDestination.Span);
+                var read = Decode(sharedInputBuffer, 0, inputBuffer.Length, sharedOutputBuffer, 0, outputBuffer.Length);
 
-                return result;
+                new ReadOnlySpan<byte>(sharedOutputBuffer, 0, read).CopyTo(outputBuffer);
+
+                return read;
             }
             finally
             {
-                ArrayPool<byte>.Shared.Return(localInputBuffer);
-                ArrayPool<byte>.Shared.Return(localOutputBuffer);
+                ArrayPool<byte>.Shared.Return(sharedInputBuffer);
+                ArrayPool<byte>.Shared.Return(sharedOutputBuffer);
             }
         }
 
@@ -69,26 +60,11 @@ namespace NextAudio
             GC.SuppressFinalize(this);
         }
 
-        /// <inheritdoc />
-        public async ValueTask DisposeAsync()
-        {
-            await DisposeAsyncCore();
-
-            Dispose(false);
-            GC.SuppressFinalize(this);
-        }
-
         /// <summary>
         /// Releases the unmanaged resources used by the <see cref="BaseDecoder" /> and optionally releases the managed resources.
         /// </summary>
         /// <param name="disposing">true to release both managed and unmanaged resources; false to release only unmanaged resources.</param>
         protected abstract void Dispose(bool disposing);
-
-        /// <summary>
-        /// Performs application-defined tasks associated with freeing, releasing, or resetting managed resources asynchronously.
-        /// </summary>
-        /// <returns> A <see cref="ValueTask" /> that represents the asynchronous dispose operation.</returns>
-        protected abstract ValueTask DisposeAsyncCore();
 
         /// <inheritdoc />
         ~BaseDecoder()
--- src/NextAudio/Decoder/IAudioDecoder.cs
+++ src/NextAudio/Decoder/IAudioDecoder.cs
@@ -7,7 +7,7 @@ namespace NextAudio
     /// <summary>
     /// Represents an audio decoder.
     /// </summary>
-    public interface IAudioDecoder : IAsyncDisposable, IDisposable
+    public interface IAudioDecoder : IDisposable
     {
         /// <summary>
         /// The channels number of the input audio.
@@ -23,22 +23,20 @@ namespace NextAudio
         /// Read and decode the input buffer and write to the output buffer.
         /// </summary>
         /// <param name="inputBuffer">A buffer containing the input audio.</param>
-        /// <param name="inputOffset">The input offset in the input buffer.</param>
+        /// <param name="inputOffset">The input offset of the input buffer.</param>
         /// <param name="inputCount">The input count of the input buffer.</param>
         /// <param name="outputBuffer">A buffer containing the output audio.</param>
         /// <param name="outputOffset">The output offset of the output buffer.</param>
         /// <param name="outputCount">The output count of the output buffer.</param>
-        /// <param name="cancellationToken">The token to monitor for cancellation requests. The default value is <see cref="CancellationToken.None"/>.</param>
-        /// <returns>The number of bytes read.</returns>
-        Task<int> DecodeAsync(byte[] inputBuffer, int inputOffset, int inputCount, byte[] outputBuffer, int outputOffset, int outputCount, CancellationToken cancellationToken = default);
+        /// <returns>The number of bytes written to the output buffer.</returns>
+        int Decode(byte[] inputBuffer, int inputOffset, int inputCount, byte[] outputBuffer, int outputOffset, int outputCount);
 
         /// <summary>
         /// Read and decode the input buffer and write to the output buffer.
         /// </summary>
         /// <param name="inputBuffer">A buffer containing the input audio.</param>
         /// <param name="outputBuffer">A buffer containing the output audio.</param>
-        /// <param name="cancellationToken">The token to monitor for cancellation requests. The default value is <see cref="CancellationToken.None"/>.</param>
-        /// <returns>The number of bytes read.</returns>
-        ValueTask<int> DecodeAsync(ReadOnlyMemory<byte> inputBuffer, Memory<byte> outputBuffer, CancellationToken cancellationToken = default);
+        /// <returns>The number of bytes written to the output buffer.</returns>
+        int Decode(ReadOnlySpan<byte> inputBuffer, Span<byte> outputBuffer);
     }
 }
