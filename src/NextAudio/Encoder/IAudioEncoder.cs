883f3403fcd593c1c7297e92e7dd660f95bbfe56
Renan Pinheiro <renancamerapinheiro@gmail.com>
2020-11-28 15:16:23 -0300

    IAudioEncoder paramrefs

--- src/NextAudio/Encoder/IAudioEncoder.cs
+++ src/NextAudio/Encoder/IAudioEncoder.cs
@@ -23,23 +23,23 @@ namespace NextAudio
         public int BitRate { get; }
 
         /// <summary>
-        /// Read and decode the input buffer and write to the output buffer.
+        /// Read and decode the <paramref name="inputBuffer" /> and write to the <paramref name="outputBuffer" />.
         /// </summary>
         /// <param name="inputBuffer">A buffer containing the input audio.</param>
-        /// <param name="inputOffset">The input offset of the input buffer.</param>
-        /// <param name="inputCount">The input count of the input buffer.</param>
+        /// <param name="inputOffset">The input offset of the <paramref name="inputBuffer" />.</param>
+        /// <param name="inputCount">The input count of the <paramref name="inputBuffer" />.</param>
         /// <param name="outputBuffer">A buffer containing the output audio.</param>
-        /// <param name="outputOffset">The output offset of the output buffer.</param>
-        /// <param name="outputCount">The output count of the output buffer.</param>
-        /// <returns>The number of bytes written to the output buffer.</returns>
+        /// <param name="outputOffset">The output offset of the <paramref name="outputBuffer" />.</param>
+        /// <param name="outputCount">The output count of the <paramref name="outputBuffer" />.</param>
+        /// <returns>The number of bytes written to the <paramref name="outputBuffer" />.</returns>
         int Encode(byte[] inputBuffer, int inputOffset, int inputCount, byte[] outputBuffer, int outputOffset, int outputCount);
 
         /// <summary>
-        /// Read and decode the input buffer and write to the output buffer.
+        /// Read and decode the <paramref name="inputBuffer" /> and write to the <paramref name="outputBuffer" />.
         /// </summary>
         /// <param name="inputBuffer">A buffer containing the input audio.</param>
         /// <param name="outputBuffer">A buffer containing the output audio.</param>
-        /// <returns>The number of bytes written to the output buffer.</returns>
+        /// <returns>The number of bytes written to the <paramref name="outputBuffer" />.</returns>
         int Encode(ReadOnlySpan<byte> inputBuffer, Span<byte> outputBuffer);
     }
 }
