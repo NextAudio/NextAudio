515d94c1ff646e3a0b7f5786c56bb8af72f28df2
Renan Pinheiro <renancamerapinheiro@gmail.com>
2020-12-02 23:45:31 -0300

    added cancellationToken overloads on IAudioProvider

--- src/NextAudio/Core/IAudioProvider.cs
+++ src/NextAudio/Core/IAudioProvider.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace NextAudio
@@ -17,21 +18,24 @@ namespace NextAudio
         /// Search for track for the specified <paramref name="query"/>.
         /// </summary>
         /// <param name="query">The query to be used for search.</param>
+        /// <param name="cancellationToken">The token to monitor for cancellation requests. The default value is <see cref="CancellationToken.None"/>.</param>
         /// <returns>The result of the search.</returns>
-        ValueTask<SearchResult> SearchAsync(string query);
+        ValueTask<SearchResult> SearchAsync(string query, CancellationToken cancellationToken = default);
 
         /// <summary>
         /// Get the audio track for the specified <see cref="AudioTrackInfo" />.
         /// </summary>
         /// <param name="trackInfo">The track info to get the audio track.</param>
+        /// <param name="cancellationToken">The token to monitor for cancellation requests. The default value is <see cref="CancellationToken.None"/>.</param>
         /// <returns>An <see cref="AudioTrack" /> instance.</returns>
-        Task<AudioTrack> GetAudioTrackAsync(AudioTrackInfo trackInfo);
+        Task<AudioTrack> GetAudioTrackAsync(AudioTrackInfo trackInfo, CancellationToken cancellationToken = default);
 
         /// <summary>
         /// Get the audio track for the specified <paramref name="identifier" />.
         /// </summary>
         /// <param name="identifier">The identifier to get the audio track.</param>
+        /// <param name="cancellationToken">The token to monitor for cancellation requests. The default value is <see cref="CancellationToken.None"/>.</param>
         /// <returns>An <see cref="AudioTrack" /> instance.</returns>
-        Task<AudioTrack> GetAudioTrackAsync(string identifier);
+        Task<AudioTrack> GetAudioTrackAsync(string identifier, CancellationToken cancellationToken = default);
     }
 }
