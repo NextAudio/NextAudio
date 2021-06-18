61b55a66f9f5bddfeabe272bbcd688d4a3996744
Renan Pinheiro <renancamerapinheiro@gmail.com>
2021-06-18 19:38:37 -0300

    AudioService provide players

--- samples/DiscordBotSample/Services/Audio/AudioService.cs
+++ samples/DiscordBotSample/Services/Audio/AudioService.cs
@@ -1,9 +1,44 @@
+using Discord.WebSocket;
+using System;
 using System.Collections.Concurrent;
+using System.Threading.Tasks;
 
 namespace DiscordBotSample.Services.Audio
 {
-    public class AudioService
+    public class AudioService : IAsyncDisposable
     {
         private readonly ConcurrentDictionary<ulong, AudioPlayer> _players;
+        private readonly DiscordSocketClient _client;
+
+        public AudioService(DiscordSocketClient client)
+        {
+            _client = client;
+
+            _players = new();
+        }
+
+        public AudioPlayer CreatePlayer(SocketGuild guild)
+        {
+            return _players.GetOrAdd(guild.Id, (_) =>
+            {
+                return new AudioPlayer(guild);
+            });
+        }
+
+        public AudioPlayer GetPlayer(SocketGuild guild)
+            => GetPlayer(guild.Id);
+
+        public AudioPlayer GetPlayer(ulong guildId)
+        {
+            _ = _players.TryGetValue(guildId, out var player);
+
+            return player;
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            foreach (var player in _players.Values)
+                await player.DisposeAsync();
+        }
     }
 }
