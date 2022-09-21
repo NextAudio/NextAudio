70ff87153504a553207a9b3268767d885ce4b5dc
Renan Pinheiro <renancamerapinheiro@gmail.com>
2022-09-20 22:57:56 -0300

    refactor: don't use reflection to check if ebml id is mapped

--- src/NextAudio.Matroska/Utils/MatroskaUtils.cs
+++ src/NextAudio.Matroska/Utils/MatroskaUtils.cs
@@ -10,7 +10,7 @@ namespace NextAudio.Matroska.Utils;
 /// </summary>
 public static class MatroskaUtils
 {
-    private static readonly Type ClrMatroskaElementType = typeof(MatroskaElementType);
+    private static readonly ulong[] MatroskaElementTypeValues = Enum.GetValues(typeof(MatroskaElementType)).Cast<ulong>().ToArray();
 
     /// <summary>
     /// Cast <see cref="MatroskaElementType" /> in an <see cref="EbmlValueType" />.
@@ -63,14 +63,40 @@ public static class MatroskaUtils
     }
 
     /// <summary>
-    /// Cast <see cref="ulong" /> in an <see cref="MatroskaElementType" />.
+    /// Check if the <paramref name="id" /> was mapped in the <see cref="MatroskaElementType" /> enum.
+    /// </summary>
+    /// <param name="id">The Ebml id to be checked.</param>
+    /// <returns><see langword="true" /> if the <paramref name="id" /> is mapped, otherwise <see langword="false" />.</returns>
+    public static bool IsEbmlIdMappedForMatroskaElementType(ulong id)
+    {
+        return MatroskaElementTypeValues.Contains(id);
+    }
+
+    /// <summary>
+    /// Cast an <see cref="ulong" /> in an <see cref="MatroskaElementType" />.
     /// </summary>
     /// <param name="id">The <see cref="ulong" /> to be cast.</param>
-    /// <returns>The casted <see cref="MatroskaElementType" />.</returns>
+    /// <returns>
+    /// The casted <see cref="MatroskaElementType" /> (will be <see cref="MatroskaElementType.Unknown" /> if <paramref name="id" /> isn't mapped).
+    /// </returns>
     public static MatroskaElementType GetMatroskaElementType(ulong id)
     {
-        return Enum.IsDefined(ClrMatroskaElementType, id)
+        return IsEbmlIdMappedForMatroskaElementType(id)
             ? (MatroskaElementType)id
             : MatroskaElementType.Unknown;
+    }
+
+    /// <summary>
+    /// Try cast an <see cref="ulong" /> in an <see cref="MatroskaElementType" />.
+    /// </summary>
+    /// <param name="id">The <see cref="ulong" /> to be cast.</param>
+    /// <param name="matroskaElementType">
+    /// The casted <see cref="MatroskaElementType" /> (will be <see cref="MatroskaElementType.Unknown" /> if the return be <see langword="false"/>).
+    /// </param>
+    /// <returns><see langword="true" /> if casted succesfully, otherwise <see langword="false" />.</returns>
+    public static bool TryGetMatroskaElementType(ulong id, out MatroskaElementType matroskaElementType)
+    {
+        matroskaElementType = GetMatroskaElementType(id);
+        return matroskaElementType != MatroskaElementType.Unknown;
     }
 }
