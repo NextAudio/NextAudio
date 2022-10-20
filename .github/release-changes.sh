dd75553389f150b115c5aa27839837a75b626538
Renan Pinheiro <renancamerapinheiro@gmail.com>
2022-10-20 06:01:10 -0300

    fix: wrong file in release-changes script

--- .github/release-changes.sh
+++ .github/release-changes.sh
@@ -6,6 +6,6 @@ body=${2//$'\n'/\\n}
 
 echo "version=$version" >> $GITHUB_ENV
 
-sed -i "4s/.*/    <VersionPrefix>$version<\/VersionPrefix>/" TestDeployment.csproj
+sed -i "4s/.*/    <VersionPrefix>$version<\/VersionPrefix>/" NextAudio.targets
 
 sed -i "5i$body\n" CHANGELOG.md
